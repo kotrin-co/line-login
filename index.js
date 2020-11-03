512f49ad5e6ed927de424c79b2ef51e9c2bbc0d5
user <user@rrcs-172-254-99-48.nyc.biz.rr.com>
2020-11-03 17:14:30 +0900

    1st commit

--- index.js
+++ index.js
@@ -26,48 +26,49 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/login', (req, res) => {
-    onetime_state_code = state_code();
+
     const query = querystring.stringify({
       response_type: 'code',
       client_id: process.env.LINECORP_PLATFORM_CHANNEL_CHANNELID,
-      redirect_uri: 'https://line-note2.herokuapp.com/callback',
-      state: onetime_state_code, // TODO: must generate random string
+      redirect_uri: 'https://line-login-kentaro.herokuapp.com/callback',
+      state: hoge, // TODO: must generate random string
       scope: 'profile',
     })
     res.redirect(301, 'https://access.line.me/oauth2/v2.1/authorize?' + query)
   })
   .get('/callback', (req, res) => {
-    const callback_state_code = req.query.state;
-    console.log('ワンタイムステート：',onetime_state_code);
-    console.log('コールバックステート：',callback_state_code);
-    request
-      .post({
-        url: `https://api.line.me/oauth2/v2.1/token`,
-        form: {
-          grant_type: "authorization_code",
-          code: req.query.code,
-          redirect_uri: 'https://line-note2.herokuapp.com/callback',
-          client_id: process.env.LINECORP_PLATFORM_CHANNEL_CHANNELID,
-          client_secret: process.env.LINECORP_PLATFORM_CHANNEL_CHANNELSECRET,
-        }
-      }, (error, response, body) => {
-        if (response.statusCode != 200) {
-          res.send(error)
-          return
-        }
-        request
-          .get({
-            url: 'https://api.line.me/v2/profile',
-            headers: {
-              'Authorization': 'Bearer ' + JSON.parse(body).access_token
-            }
-          }, (error, response, body) => {
-            if (response.statusCode != 200) {
-              res.send(error)
-              return
-            }
-            res.render('pages/index')
-          })
-      })
+    res.send('code:'+req.query.code);
+    // const callback_state_code = req.query.state;
+    // console.log('ワンタイムステート：',onetime_state_code);
+    // console.log('コールバックステート：',callback_state_code);
+    // request
+    //   .post({
+    //     url: `https://api.line.me/oauth2/v2.1/token`,
+    //     form: {
+    //       grant_type: "authorization_code",
+    //       code: req.query.code,
+    //       redirect_uri: 'https://line-note2.herokuapp.com/callback',
+    //       client_id: process.env.LINECORP_PLATFORM_CHANNEL_CHANNELID,
+    //       client_secret: process.env.LINECORP_PLATFORM_CHANNEL_CHANNELSECRET,
+    //     }
+    //   }, (error, response, body) => {
+    //     if (response.statusCode != 200) {
+    //       res.send(error)
+    //       return
+    //     }
+    //     request
+    //       .get({
+    //         url: 'https://api.line.me/v2/profile',
+    //         headers: {
+    //           'Authorization': 'Bearer ' + JSON.parse(body).access_token
+    //         }
+    //       }, (error, response, body) => {
+    //         if (response.statusCode != 200) {
+    //           res.send(error)
+    //           return
+    //         }
+    //         res.render('pages/index')
+    //       })
+    //   })
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
